cd1b13d5211c7fbca1da5d975c45ec1a2214e485
Raj-RapidQuest <rajrathod1998@gmail.com>
2021-04-14 00:28:37 +0530

    First Commit

---
+++ RapidTest/RapidTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapidTestApp.swift
+//  RapidTest
+//
+//  Created by admin on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct RapidTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
